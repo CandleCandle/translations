1b1994918deb39ddd3299de39f2652822a9aaa36
Candle <candle@candle.me.uk>
2010-09-09 20:53:48 +0200

    works for single methods.

--- src/main/java/uk/me/candle/translations/App.java
+++ src/main/java/uk/me/candle/translations/App.java
@@ -1,5 +1,6 @@
 package uk.me.candle.translations;
 
+import uk.me.candle.translations.bundle.Names;
 import uk.me.candle.translations.foo.Foo;
 
 
@@ -12,10 +13,10 @@ public class App {
   public static void main(String[] args) {
     System.out.println("trn:" + Foo.get().bar());
 
-//    System.out.println("Title trn : " + Names.get().title());
-//    System.out.println("Password trn : " + Names.get().password());
-//    System.out.println("Username trn : " + Names.get().username());
-//    System.out.println("One param trn : " + Names.get().param("some"));
-//    System.out.println("Two params trn : " + Names.get().param("some", "params"));
+    System.out.println("Title trn : " + Names.get().title());
+    System.out.println("Password trn : " + Names.get().password());
+    System.out.println("Username trn : " + Names.get().username());
+    System.out.println("One param trn : " + Names.get().param("some"));
+    System.out.println("Two params trn : " + Names.get().param("some", "params"));
   }
 }
