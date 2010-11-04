f96a31753ecb09e596388cf49fa687628371db14
Candle <candle@candle.me.uk>
2010-11-04 16:18:43 +0100

    Fixed the formatting and javadoc comments.

--- src/test/java/uk/me/candle/translations/example/FooTest.java
+++ src/test/java/uk/me/candle/translations/example/FooTest.java
@@ -13,16 +13,15 @@ import static org.junit.Assert.*;
  * @author Candle
  */
 public class FooTest {
-  @Test
-  public void testFooBundle_en() throws Exception {
-    // Load the bundle with the check flags enabled.
-    // If there is an error, in either the abstract class
-    // or the properties file has an error.
-    Foo foo = Bundle.load(Foo.class, Locale.ENGLISH,
-            LoadIgnoreMissing.NO,
-            LoadIgnoreExtra.NO,
-            LoadIgnoreParameterMisMatch.NO
-            );
-    assertNotNull(foo.bar());
-  }
+	@Test
+	public void testFooBundle_en() throws Exception {
+		// Load the bundle with the check flags enabled.
+		// this will throw an exception if there is an error
+		// with either the abstract Class or the properties file
+		Foo foo = Bundle.load(Foo.class, Locale.ENGLISH,
+				LoadIgnoreMissing.NO,
+				LoadIgnoreExtra.NO,
+				LoadIgnoreParameterMisMatch.NO);
+		assertNotNull(foo.bar());
+	}
 }
